dabd3b4aa8983d61aa5438b1cd5408b65d6e7b54
Edge Opsx <edgeops@ust-global.com>
2023-12-11 09:24:43 +0000

    REG137-VENUS-PROJ-1-STANDARD-TEMPLATE-00: Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx:alpine
+
+COPY ./dist/reg137-venus-proj-1-standard-template/ /usr/share/nginx/html
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  basePath: `https://reg137-venus-proj-1-standard-template-backend-argocd.dagility.com/`,
+};
